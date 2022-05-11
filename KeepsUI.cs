95c776b8dc3e4efc85cc170c872b4a497bfdd490
Nicholas Denaro <ndenarodev@gmail.com>
2022-05-10 23:10:01 -0400

    Make UI more extensible Add config

--- KeepsUI.cs
+++ KeepsUI.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Terraria;
 using Terraria.GameContent.UI.Elements;
 using Terraria.ID;
@@ -11,29 +14,9 @@ namespace NDayCycle
     {
         private MenuBar menu;
 
-        private Item pickaxeItem;
-        private UIImage pickaxeItemImage;
-        private UIText pickaxeItemCount;
+        private List<ItemSlot> slots = new List<ItemSlot>();
 
-        private Item axeItem;
-        private UIImage axeItemImage;
-        private UIText axeItemCount;
-
-        private Item weaponItem;
-        private UIImage weaponItemImage;
-        private UIText weaponItemCount;
-
-        private Item item1;
-        private UIImage item1Image;
-        private UIText item1Count;
-
-        private Item item2;
-        private UIImage item2Image;
-        private UIText item2Count;
-
-        private Item item3;
-        private UIImage item3Image;
-        private UIText item3Count;
+        public int SlotsCount => slots.Count;
 
         public KeepsUI(MenuBar menu)
         {
@@ -44,143 +27,131 @@ namespace NDayCycle
         {
             base.OnInitialize();
 
-            const int imgOffset = 12;
-            const float imgScale = 1.25f;
-            const int txtOffset = 30;
+            slots.Add(new ItemSlot(0, 0, this, ItemSlot.SlotType.pickaxe));
+            slots.Add(new ItemSlot(50, 0, this, ItemSlot.SlotType.axe));
+            slots.Add(new ItemSlot(100, 0, this, ItemSlot.SlotType.hammer));
+            slots.Add(new ItemSlot(150, 0, this, ItemSlot.SlotType.weapon));
 
-            pickaxeItem = new Item();
-            axeItem = new Item();
-            weaponItem = new Item();
-            item1 = new Item();
-            item2 = new Item();
-            item3 = new Item();
+            for (int i = 0; i < ModContent.GetInstance<NDayCycleConfig>().MiscItemsToKeep; i++)
+            {
+                slots.Add(new ItemSlot(50 * (i % 3), 50 * (1 + i / 3), this, ItemSlot.SlotType.any));
+            }
 
-            this.Width.Set(150, 0);
-            this.Height.Set(100, 0);
+            //slots.Add(new ItemSlot(0, 50, this, ItemSlot.SlotType.any));
+            //slots.Add(new ItemSlot(50, 50, this, ItemSlot.SlotType.any));
+            //slots.Add(new ItemSlot(100, 50, this, ItemSlot.SlotType.any));
 
-            var img = new UIImage(ModContent.GetTexture("Terraria/Inventory_Back2"));
-            img.Left.Set(0, 0);
-            img.OnClick += (e, l) => Img_OnClick(e, l, ref pickaxeItem, ref pickaxeItemImage, ref pickaxeItemCount);
-            this.Append(img);
-            var tool = new UIImage(Main.itemTexture[ItemID.CopperPickaxe]);
-            tool.Left.Set(0 - 4, 0);
-            tool.Top.Set(0 - 4, 0);
-            tool.ImageScale = 0.5f;
-            this.Append(tool);
-            pickaxeItemImage = new UIImage(Main.itemTexture[0]);
-            pickaxeItemImage.Left.Set(0 + imgOffset, 0);
-            pickaxeItemImage.Top.Set(0 + imgOffset, 0);
-            pickaxeItemImage.ImageScale = imgScale;
-            this.Append(pickaxeItemImage);
-            pickaxeItemCount = new UIText("");
-            pickaxeItemCount.Left.Set(0 + imgOffset, 0);
-            pickaxeItemCount.Top.Set(0 + txtOffset, 0);
-            this.Append(pickaxeItemCount);
-
-            img = new UIImage(ModContent.GetTexture("Terraria/Inventory_Back2"));
-            img.Left.Set(50, 0);
-            img.OnClick += (e, l) => Img_OnClick(e, l, ref axeItem, ref axeItemImage, ref axeItemCount);
-            this.Append(img);
-            tool = new UIImage(Main.itemTexture[ItemID.CopperAxe]);
-            tool.Left.Set(50 - 4, 0);
-            tool.Top.Set(0 - 4, 0);
-            tool.ImageScale = 0.5f;
-            this.Append(tool);
-            axeItemImage = new UIImage(Main.itemTexture[0]);
-            axeItemImage.Left.Set(50 + imgOffset, 0);
-            axeItemImage.Top.Set(0 + imgOffset, 0);
-            axeItemImage.ImageScale = imgScale;
-            this.Append(axeItemImage);
-            axeItemCount = new UIText("");
-            axeItemCount.Left.Set(50 + imgOffset, 0);
-            axeItemCount.Top.Set(0 + txtOffset, 0);
-            this.Append(axeItemCount);
-
-            img = new UIImage(ModContent.GetTexture("Terraria/Inventory_Back2"));
-            img.Left.Set(100, 0);
-            img.OnClick += (e, l) => Img_OnClick(e, l, ref weaponItem, ref weaponItemImage, ref weaponItemCount);
-            this.Append(img);
-            tool = new UIImage(Main.itemTexture[ItemID.CopperShortsword]);
-            tool.Left.Set(100 - 4, 0);
-            tool.Top.Set(0 - 4, 0);
-            tool.ImageScale = 0.5f;
-            this.Append(tool);
-            weaponItemImage = new UIImage(Main.itemTexture[0]);
-            weaponItemImage.Left.Set(100 + imgOffset, 0);
-            weaponItemImage.Top.Set(0 + imgOffset, 0);
-            weaponItemImage.ImageScale = imgScale;
-            this.Append(weaponItemImage);
-            weaponItemCount = new UIText("");
-            weaponItemCount.Left.Set(100 + imgOffset, 0);
-            weaponItemCount.Top.Set(0 + txtOffset, 0);
-            this.Append(weaponItemCount);
-
-            img = new UIImage(ModContent.GetTexture("Terraria/Inventory_Back2"));
-            img.Left.Set(0, 0);
-            img.Top.Set(50, 0);
-            img.OnClick += (e, l) => Img_OnClick(e, l, ref item1, ref item1Image, ref item1Count);
-            this.Append(img);
-            item1Image = new UIImage(Main.itemTexture[0]);
-            item1Image.Left.Set(0 + imgOffset, 0);
-            item1Image.Top.Set(50 + imgOffset, 0);
-            item1Image.ImageScale = imgScale;
-            this.Append(item1Image);
-            item1Count = new UIText("");
-            item1Count.Left.Set(0 + imgOffset, 0);
-            item1Count.Top.Set(50 + txtOffset, 0);
-            this.Append(item1Count);
-
-            img = new UIImage(ModContent.GetTexture("Terraria/Inventory_Back2"));
-            img.Left.Set(50, 0);
-            img.Top.Set(50, 0);
-            img.OnClick += (e, l) => Img_OnClick(e, l, ref item2, ref item2Image, ref item2Count);
-            this.Append(img);
-            item2Image = new UIImage(Main.itemTexture[0]);
-            item2Image.Left.Set(50 + imgOffset, 0);
-            item2Image.Top.Set(50 + imgOffset, 0);
-            item2Image.ImageScale = imgScale;
-            this.Append(item2Image);
-            item2Count = new UIText("");
-            item2Count.Left.Set(50 + imgOffset, 0);
-            item2Count.Top.Set(50 + txtOffset, 0);
-            this.Append(item2Count);
-
-            img = new UIImage(ModContent.GetTexture("Terraria/Inventory_Back2"));
-            img.Left.Set(100, 0);
-            img.Top.Set(50, 0);
-            img.OnClick += (e, l) => Img_OnClick(e, l, ref item3, ref item3Image, ref item3Count);
-            this.Append(img);
-            item3Image = new UIImage(Main.itemTexture[0]);
-            item3Image.Left.Set(100 + imgOffset, 0);
-            item3Image.Top.Set(50 + imgOffset, 0);
-            item3Image.ImageScale = imgScale;
-            this.Append(item3Image);
-            item3Count = new UIText("");
-            item3Count.Left.Set(100 + imgOffset, 0);
-            item3Count.Top.Set(50 + txtOffset, 0);
-            this.Append(item3Count);
+            this.Width.Set(200, 0);
+            this.Height.Set(50 + 50 * (1 + ModContent.GetInstance<NDayCycleConfig>().MiscItemsToKeep / 3), 0);
 
             this.Recalculate();
         }
 
-        private void Img_OnClick(UIMouseEvent evt, UIElement listeningElement, ref Item item, ref UIImage img, ref UIText txt)
+        public override void Update(GameTime gameTime)
         {
-            if (menu.IsReady)
+            base.Update(gameTime);
+            Main.LocalPlayer.mouseInterface = true;
+        }
+
+        public void SetItems()
+        {
+            for (int i = 0; i < slots.Count; i++)
+            {
+                Main.player[Main.myPlayer].inventory[i] = slots[i].ItemClone;
+                slots[i].TurnToAir();
+            }
+        }
+    }
+
+    public class ItemSlot
+    {
+        public enum SlotType { pickaxe = ItemID.CopperPickaxe, axe = ItemID.CopperAxe, hammer = ItemID.CopperHammer, weapon = ItemID.CopperShortsword, any = 0 }
+
+        private Item item;
+        private UIImage image;
+        private UIText count;
+        private SlotType type;
+
+        public Item Item => item;
+        public Item ItemClone => item.DeepClone();
+
+        private bool enabled;
+
+        const int imgOffset = 12;
+        const float imgScale = 1.25f;
+        const int txtOffset = 30;
+        public ItemSlot(int x, int y, UIElement parent, SlotType slot)
+        {
+            this.item = new Item();
+            this.image = new UIImage(Main.itemTexture[0]);
+            this.count = new UIText("");
+            this.enabled = true;
+            this.type = slot;
+
+            var img = new UIImage(ModContent.GetTexture("Terraria/Inventory_Back2"));
+            img.Left.Set(x, 0);
+            img.Top.Set(y, 0);
+            img.OnClick += Img_OnClick;
+            parent.Append(img);
+
+            if (type != SlotType.any)
+            {
+                var tool = new UIImage(Main.itemTexture[(int)type]);
+                tool.Left.Set(x - 4, 0);
+                tool.Top.Set(y - 4, 0);
+                tool.ImageScale = 0.5f;
+                parent.Append(tool);
+            }
+
+            image = new UIImage(Main.itemTexture[0]);
+            image.Left.Set(x + imgOffset, 0);
+            image.Top.Set(y + imgOffset, 0);
+            image.ImageScale = imgScale;
+            parent.Append(image);
+
+            count = new UIText("");
+            count.Left.Set(x + imgOffset, 0);
+            count.Top.Set(y + txtOffset, 0);
+            parent.Append(count);
+        }
+
+        public void Disable()
+        {
+            this.enabled = false;
+        }
+
+        public void Enable()
+        {
+            this.enabled = true;
+        }
+
+        public void TurnToAir()
+        {
+            this.item.TurnToAir();
+            image.SetImage(Main.itemTexture[0]);
+            count.SetText("");
+        }
+
+        private void Img_OnClick(UIMouseEvent evt, UIElement listeningElement)
+        {
+            if (!this.enabled)
             {
                 return;
             }
 
-            //Main.NewText($"keepsui {Main.mouseItem.ToString()}");
-
-            if (item == pickaxeItem && !Main.mouseItem.IsAir && Main.mouseItem.pick == 0)
+            if (type == SlotType.pickaxe && !Main.mouseItem.IsAir && Main.mouseItem.pick <= 0)
             {
                 return;
             }
-            else if (item == axeItem && !Main.mouseItem.IsAir && Main.mouseItem.axe == 0)
+            else if (type == SlotType.axe && !Main.mouseItem.IsAir && Main.mouseItem.axe <= 0)
             {
                 return;
             }
-            else if (item == weaponItem && !Main.mouseItem.IsAir && Main.mouseItem.damage <= 0)
+            else if (type == SlotType.hammer && !Main.mouseItem.IsAir && Main.mouseItem.hammer <= 0)
+            {
+                return;
+            }
+            else if (type == SlotType.weapon && !Main.mouseItem.IsAir && Main.mouseItem.damage <= 0)
             {
                 return;
             }
@@ -190,55 +161,8 @@ namespace NDayCycle
             item = temp;
             Main.isMouseLeftConsumedByUI = true;
 
-            img.SetImage(Main.itemTexture[item.type]);
-            txt.SetText(item.stack > 1 ? "" + item.stack : "");
-        }
-
-        public override void Update(GameTime gameTime)
-        {
-            base.Update(gameTime);
-            Main.LocalPlayer.mouseInterface = true;
-        }
-
-        public void DeepCloneItems()
-        {
-            pickaxeItem = pickaxeItem.DeepClone();
-            axeItem = axeItem.DeepClone();
-            weaponItem = weaponItem.DeepClone();
-            item1 = item1.DeepClone();
-            item2 = item2.DeepClone();
-            item3 = item3.DeepClone();
-        }
-
-        public void SetItems()
-        {
-            Main.player[Main.myPlayer].inventory[0] = pickaxeItem.DeepClone();
-            Main.player[Main.myPlayer].inventory[1] = axeItem.DeepClone();
-            Main.player[Main.myPlayer].inventory[2] = weaponItem.DeepClone();
-            Main.player[Main.myPlayer].inventory[3] = item1.DeepClone();
-            Main.player[Main.myPlayer].inventory[4] = item2.DeepClone();
-            Main.player[Main.myPlayer].inventory[5] = item3.DeepClone();
-
-            pickaxeItem.TurnToAir();
-            axeItem.TurnToAir();
-            weaponItem.TurnToAir();
-            item1.TurnToAir();
-            item2.TurnToAir();
-            item3.TurnToAir();
-
-            pickaxeItemImage.SetImage(Main.itemTexture[0]);
-            axeItemImage.SetImage(Main.itemTexture[0]);
-            weaponItemImage.SetImage(Main.itemTexture[0]);
-            item1Image.SetImage(Main.itemTexture[0]);
-            item2Image.SetImage(Main.itemTexture[0]);
-            item3Image.SetImage(Main.itemTexture[0]);
-
-            pickaxeItemCount.SetText("");
-            axeItemCount.SetText("");
-            weaponItemCount.SetText("");
-            item1Count.SetText("");
-            item2Count.SetText("");
-            item3Count.SetText("");
+            image.SetImage(Main.itemTexture[item.type]);
+            count.SetText(item.stack > 1 ? "" + item.stack : "");
         }
     }
 
@@ -261,8 +185,6 @@ namespace NDayCycle
 
             var resetButton = new UIImageButton(ModContent.GetTexture("Terraria/UI/ButtonPlay"));
             resetButton.HAlign = resetButton.VAlign = 1.0f;
-            //resetButton.Left.Set(150 - 30, 0);
-            //resetButton.Top.Set(100 - 30, 0);
             resetButton.OnClick += ResetButton_OnClick;
             resetButton.Recalculate();
             panel.Append(resetButton);
@@ -290,19 +212,28 @@ namespace NDayCycle
         public void ResetInventory()
         {
             this.IsReady = false;
-            keeps.DeepCloneItems();
-
-            foreach (Item item in Main.player[Main.myPlayer].inventory)
-            {
-                item.TurnToAir();
-            }
-
-            foreach (Item item in Main.player[Main.myPlayer].armor)
-            {
-                item.TurnToAir();
-            }
 
             keeps.SetItems();
+
+            foreach (Item item in Main.player[Main.myPlayer].inventory.Skip(keeps.SlotsCount))
+            {
+                if ((item.type == ItemID.CopperCoin || item.type == ItemID.SilverCoin || item.type == ItemID.GoldCoin || item.type == ItemID.PlatinumCoin) && ModContent.GetInstance<NDayCycleConfig>().KeepMoney)
+                {
+                    continue;
+                }
+
+                item.TurnToAir();
+            }
+
+            foreach (Item item in Main.player[Main.myPlayer].armor.Skip(ModContent.GetInstance<NDayCycleConfig>().KeepEquips ? 3 : 0))
+            {
+                if (item.accessory && ModContent.GetInstance<NDayCycleConfig>().KeepAccessories)
+                {
+                    continue;
+                }
+                item.TurnToAir();
+            }
+
 
             this.Deactivate();
             NDayCycle.HideMenu();
@@ -312,13 +243,6 @@ namespace NDayCycle
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-
-            //Main.LocalPlayer.mouseInterface = true;
-        }
-
-        public override void Click(UIMouseEvent evt)
-        {
-            //Main.NewText($"menubar {Main.mouseItem.ToString()}");
         }
     }
 }
---
+++ NDayCycleConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace NDayCycle
+{
+    class NDayCycleConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Label("Keep equipment after reset")]
+        [Tooltip("When enabled, players will keep their equipment after the world is reset")]
+        [DefaultValue(false)]
+        public bool KeepEquips { get; set; }
+
+        [Label("Keep accessories after reset")]
+        [Tooltip("When enabled, players will keep their accessories after the world is reset")]
+        [DefaultValue(false)]
+        public bool KeepAccessories { get; set; }
+
+        [Label("Keep money after reset")]
+        [Tooltip("When enabled, players will keep their money after the world is reset")]
+        [DefaultValue(false)]
+        public bool KeepMoney { get; set; }
+
+        [Label("Disallow items that are stackable")]
+        [Tooltip("When enabled, players will not be allowed to keep stackable items after the world is reset")]
+        [DefaultValue(false)]
+        public bool DisableStackableItems { get; set; }
+
+        [ReloadRequired]
+        [Label("Specifies the number additional items to keep")]
+        [Tooltip("Allows to customize additional items slots to keep after the world is reset (between 0 and 6)")]
+        [DefaultValue(3)]
+        public int MiscItemsToKeep { get; set; }
+
+
+    }
+}
